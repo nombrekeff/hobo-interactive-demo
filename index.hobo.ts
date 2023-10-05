9c896e7086b832688e72a056401b560ab8403927
Manolo Edge <nombrekeff@rallf.com>
2023-10-05 16:28:17 +0200

    feat: improved demo style

--- index.hobo.ts
+++ index.hobo.ts
@@ -2,10 +2,28 @@ import * as fs from 'fs';
 import path from 'path';
 import { builders, generate, doc } from 'hobo-js/src/hobo';
 
-const { script, h3, h4, div, hr, style, p } = builders;
+const { script, h3, h4, div, hr, style, p, link, a, br } = builders;
 
 const indexDoc = doc('Hobo Interactive Demo');
-style.attach({
+indexDoc.head.append(link.aa('href', 'https://fonts.googleapis.com'));
+indexDoc.head.append(
+  link.aa('href', 'https://fonts.googleapis.com/css2?family=AR+One+Sans:wght@400;500;600;700&display=swap'),
+);
+indexDoc.head.append(link.aa('href', 'https://yarnpkg.com/en/package/normalize.css'));
+
+style.attach.build({
+  ':root': {
+    fontFamily: "'AR One Sans', sans-serif",
+    color: '#333',
+  },
+  h4: {
+    margin: '8px 0',
+    padding: '0',
+  },
+  p: {
+    color: '#666',
+    margin: '8px 0',
+  },
   '.cm-editor': {
     minHeight: '15em',
     maxHeight: '50em',
@@ -14,10 +32,26 @@ style.attach({
     borderRadius: '8px',
     wordWrap: 'break-word',
   },
+  '.space-between': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  'hr':{
+    border: '1px solid #ddd',
+  }
 });
 
-h3.attach('Welcome to Hobo demo');
-hr.build();
+div.attach.addClass('space-between')(
+  h3('Welcome to Hobo Interactive Demo'),
+  div.b(
+    a.addAttr('href', 'https://github.com/nombrekeff/hobo-js')('GitHub'),
+    ' ',
+    a.addAttr('href', 'https://nombrekeff.github.io/hobo-js/')('Docs'),
+  ),
+);
+hr.attach.build();
+br.attach.build();
 
 h4.attach('Write hobo code');
 p.attach('You must return a tag to generate it');
